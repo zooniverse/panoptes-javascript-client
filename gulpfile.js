74a7ec5154d73403cee1668a6fef94ec5da5b761
Chris Snyder <bumishness@gmail.com>
2015-09-11 18:06:49 -0500

    Simplify gulpfile, remove unused dependencies, use superagent json-api-client

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,25 @@
-var gulp = require("gulp");
-var mocha = require("gulp-mocha");
-var babel = require("babel/register");
-var browserify = require("browserify");
-var babelify = require("babelify");
-var source = require('vinyl-source-stream');
-var watchify = require("watchify");
+require('babel/register');
+
+var gulp = require('gulp');
+
+var babel = require('gulp-babel');
+var mocha = require('gulp-mocha');
 
 var config = {
   entryFile: './src/client.js',
-  outputDir: './build/',
-  outputFile: 'bundle.js',
   testFiles: 'test/**/*.js',
-  srcFiles: 'src/**/*.js'
+  srcFiles: 'src/**/*.js',
+  outDir: './lib'
 };
 
-var bundler;
-function getBundler() {
-  if (!bundler) {
-    var options = Object.create(watchify.args);
-    options.debug = true;
-    bundler = watchify(browserify(config.entryFile, options));
-  }
-  return bundler;
-};
-
-function bundle() {
-  return getBundler()
-    .transform(babelify)
-    .bundle()
-    .on('error', function(err) { console.log('Error: ' + err.message); })
-    .pipe(source(config.outputFile))
-    .pipe(gulp.dest(config.outputDir))
-}
-
-gulp.task('build', function(){
-  return bundle();
+gulp.task('build', function() {
+  return gulp.src([config.srcFiles])
+    .pipe(babel())
+    .pipe(gulp.dest(config.outDir));
 });
 
-gulp.task('compile', ['build'] ,function(){
-  process.exit(0);
+gulp.task('watch', ['build'], function() {
+  return gulp.watch([config.srcFiles], ['build']);
 });
 
 gulp.task('watch-test', function() {
@@ -55,10 +36,4 @@ gulp.task('test', function() {
     .once('end', function() {
       process.exit(0);
     });
-});
-
-gulp.task('watch', ['build'], function(){
-  getBundler().on('update', function() {
-    gulp.start('build');
-  });
 });
