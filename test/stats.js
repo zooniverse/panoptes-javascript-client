99739177ccf84d1348e8ffaf73b8fd2212d2959d
Brian Carstensen <brian.carstensen@gmail.com>
2016-05-09 10:23:18 -0500

    Tweak stats client for consistency, stub out a test

---
+++ test/stats.js
@@ -0,0 +1,21 @@
+var test = require('blue-tape');
+var statsClient = require('../lib/stats-client');
+
+var GALAXY_ZOO_BARS_ID = '3';
+var GALAXY_ZOO_BARS_WORKFLOW_ID = '1623';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('We can currently only tests the stats client in production.');
+  process.exit(1);
+}
+
+test('We can get stats for a project', function(t) {
+  return statsClient.query({
+    project_id: GALAXY_ZOO_BARS_ID,
+    workflow_id: GALAXY_ZOO_BARS_WORKFLOW_ID,
+    type: 'classification',
+    period: 'day'
+  }).then(function(results) {
+    return t.ok(Array.isArray(results));
+  });
+});
