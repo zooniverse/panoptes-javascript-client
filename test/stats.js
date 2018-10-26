a9d6ff66d14c0442b89b8385de1a63b441dba26d
mcbouslog <mcbouslog@gmail.com>
2018-10-26 15:29:50 -0500

    Update stats test

--- test/stats.js
+++ test/stats.js
@@ -3,6 +3,7 @@ var statsClient = require('../lib/stats-client');
 
 var GALAXY_ZOO_BARS_ID = '3';
 var GALAXY_ZOO_BARS_WORKFLOW_ID = '1623';
+var USER_ID = '1459668'; // user "zootester1" on production
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('We can currently only tests the stats client in production.');
@@ -11,10 +12,11 @@ if (process.env.NODE_ENV !== 'production') {
 
 test('We can get stats for a project', function(t) {
   return statsClient.query({
+    period: 'day',
     projectID: GALAXY_ZOO_BARS_ID,
-    workflowID: GALAXY_ZOO_BARS_WORKFLOW_ID,
     type: 'classification',
-    period: 'day'
+    userID: USER_ID,
+    workflowID: GALAXY_ZOO_BARS_WORKFLOW_ID
   }).then(function(results) {
     return t.ok(Array.isArray(results), 'Should get an array back');
   });
