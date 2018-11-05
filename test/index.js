a34c2a2f3ba1630cddc602273c4efa61287ba219
Mark Bouslog <mcbouslog@gmail.com>
2018-11-05 13:44:06 -0600

    Update stats-client for user_id query (#105)
    * Update stats-client with user_id, multiple params
    
    * Refactor stats tests

--- test/index.js
+++ test/index.js
@@ -1,1 +1,2 @@
 require('./auth');
+require('./stats');
--- test/stats.js
+++ test/stats.js
@@ -1,20 +1,46 @@
 var test = require('blue-tape');
 var statsClient = require('../lib/stats-client');
 
-var GALAXY_ZOO_BARS_ID = '3';
-var GALAXY_ZOO_BARS_WORKFLOW_ID = '1623';
-
-if (process.env.NODE_ENV !== 'production') {
-  console.log('We can currently only tests the stats client in production.');
-  process.exit(1);
-}
+var PROJECT_ID = '335'; // staging project "I Fancy Cats" ID = 335, production project "Galaxy Zoo: Bar Lengths" ID = 3
+var WORKFLOW_ID = '693'; // staging workflow "I Fancy Cats - Cat me!" ID = 693, production workflow "Galaxy Zoo: Bar Lengths - Find Off-Center Bars" ID = 1623
+var USER_ID = '1325801'; // staging "zootester1" ID = 1325801, production "zootester1" ID = 1459668
 
 test('We can get stats for a project', function(t) {
   return statsClient.query({
-    projectID: GALAXY_ZOO_BARS_ID,
-    workflowID: GALAXY_ZOO_BARS_WORKFLOW_ID,
+    period: 'day',
+    projectID: PROJECT_ID,
+    type: 'classification'
+  }).then(function(results) {
+    return t.ok(Array.isArray(results), 'Should get an array back');
+  });
+});
+
+test('We can get stats for a workflow', function(t) {
+  return statsClient.query({
+    period: 'day',
     type: 'classification',
-    period: 'day'
+    workflowID: WORKFLOW_ID
+  }).then(function(results) {
+    return t.ok(Array.isArray(results), 'Should get an array back');
+  });
+});
+
+test('We can get stats for a user', function(t) {
+  return statsClient.query({
+    period: 'day',
+    type: 'classification',
+    userID: USER_ID
+  }).then(function(results) {
+    return t.ok(Array.isArray(results), 'Should get an array back');
+  });
+});
+
+test('We can get stats for a user by project', function(t) {
+  return statsClient.query({
+    period: 'day',
+    projectID: PROJECT_ID,
+    type: 'classification',
+    userID: USER_ID
   }).then(function(results) {
     return t.ok(Array.isArray(results), 'Should get an array back');
   });
