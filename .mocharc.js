af1c3a02cf9a50b018f5e58955739d3994763e2f
Jim O'Donnell <jim@zooniverse.org>
2023-01-27 18:08:13 +0000

    Add SugarClient tests (#192)
    - move the old tests to `/tape-test`.
    - add the SugarClient tests from `zooniverse/Sugar-Client`.
    - install `mocha`, `chai` and `chai-spies`.

---
+++ .mocharc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  reporter: 'spec',
+  checkLeaks: true,
+  slow: 300
+}
---
+++ test/support/mock_primus.js
@@ -0,0 +1,10 @@
+var MockPrimus = {
+  connect() { return MockPrimus; },
+  on() { return MockPrimus; },
+  off() { return MockPrimus; },
+  open() { return MockPrimus; },
+  end() { return MockPrimus; },
+  write() { return MockPrimus; }
+};
+
+module.exports = MockPrimus;
