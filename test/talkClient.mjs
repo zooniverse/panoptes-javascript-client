de1900884a2f07c7ea76578c274296bb9f960013
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-03-28 13:58:32 +0000

    test individual comments

--- test/talkClient.mjs
+++ test/talkClient.mjs
@@ -14,6 +14,10 @@ describe('talkClient', function () {
       const results = await talkClient.type('searches').get(params);
       expect(results).to.be.an('array');
       expect(results).to.have.lengthOf(10);
+      results.forEach(result => {
+        expect(result.type).to.equal('Comment');
+        expect(result.body).to.be.a('string');
+      });
     });
     specify('should ignore null comments', async function () {
       // this Talk query returns 9 comments and one null comment.
@@ -27,6 +31,10 @@ describe('talkClient', function () {
       const results = await talkClient.type('searches').get(params);
       expect(results).to.be.an('array');
       expect(results).to.have.lengthOf(9);
+      results.forEach(result => {
+        expect(result.type).to.equal('Comment');
+        expect(result.body).to.be.a('string');
+      });
     });
   });
 });
